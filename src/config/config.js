ac591cbddabd0aaf227c3137f4f60a05e75b9bca
enrique <kikeortizjrv65@gmail.com>
2022-01-16 14:36:21 -0600

    cambios

--- src/config/config.js
+++ src/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
 	db_port: process.env.DB_PORT || 3306,
 	db_usuario: process.env.DB_USUARIO || 'root',
 	db_password: process.env.DB_PASSWORD || '',
-	db_name: process.env.DB_NAME || 'anf175',
+	db_name: process.env.DB_NAME || 'anf176',
 	db_host: process.env.DB_HOST || 'localhost',
 	db_dialect: process.env.DB_DIALECT || 'mysql',
 	session_key: process.env.SESSION_KEY || 'abc',
